4551494ef4097ebd4aa03f2e5ffcc5a9bb56fb73
adityasays <aditya.shukla019r@gmail.com>
2023-09-07 15:22:15 +0530

    logic improved

--- logic.js
+++ logic.js
@@ -1,51 +1,85 @@
-// Select all option elements and buttons
-const options = document.querySelectorAll('.option');
-const saveImageBtn = document.querySelector('.saveimage');
-let fltname=document.querySelector(".details .name")
-
-// Function to handle when an option is clicked
-function handleOptionClick(event) {
-    const selectedEffect = event.target.getAttribute('data-effect');
-    document.querySelector(".textbox").value = selectedEffect;
-    console.log(selectedEffect)
-    
-    // Perform any other actions for the selected effect here
-fltname.innerText = selectedEffect;
-}
-
-// Add click event listeners to each option
-options.forEach(option => {
-    option.addEventListener('click', handleOptionClick);
-});
-
-
-// Event listener for the "Save Image" button
-saveImageBtn.addEventListener('click', () => {
-    // Code to save the edited image goes here
-});
-
-let fileInput = document.querySelector(".file-input");
-let chooseImg = document.querySelector(".chooseimage");
-let preview = document.querySelector(".image_container img");
-
-function loadImg(){
-    let file=fileInput.files[0];
-    if(!file)
-    {
-        return;
+document.addEventListener("DOMContentLoaded", function () {
+   
+    const options = document.querySelectorAll('.option');
+    const saveImageBtn = document.querySelector('.saveimage');
+    let fltname = document.getElementsByClassName("name")[0];
+    let brightness=100 , saturation=100, inversion=0,greyscale=0;
+    function handleOptionClick(event) {
+      document.querySelector(".active").classList.remove("active");
+      event.target.classList.add("active");
+      const selectedEffect = event.target.getAttribute('data-effect');
+      document.querySelector(".textbox").value = selectedEffect;
+      fltname.innerText = selectedEffect;
+      if (selectedEffect === "brightness") {
+        filterslider.value = brightness;
+        values.innerHTML = `&nbsp ${filterslider.value} % `;
+      } else if (selectedEffect === "saturation") {
+        filterslider.value = saturation;
+        values.innerHTML = `&nbsp ${filterslider.value} % `;
+      } else if (selectedEffect === "greyscale") {
+        filterslider.value = greyscale;
+        values.innerHTML = `&nbsp ${filterslider.value} % `;
+      } else if (selectedEffect === "inversion") {
+        filterslider.value = inversion;
+        values.innerHTML = `&nbsp ${filterslider.value} % `;
+      }
     }
-    preview.src = URL.createObjectURL(file);
-    preview.addEventListener("load",()=>{
+    
+  
+
+    options.forEach(option => {
+      option.addEventListener('click', handleOptionClick);
+    });
+  
+    
+    saveImageBtn.addEventListener('click', () => {
+      
+    });
+  
+    let fileInput = document.querySelector(".file-input");
+    let chooseImg = document.querySelector(".chooseimage");
+    let preview = document.querySelector(".image_container img");
+  
+    function loadImg() {
+      let file = fileInput.files[0];
+      if (!file) {
+        return;
+      }
+      preview.src = URL.createObjectURL(file);
+      preview.addEventListener("load", () => {
         document.querySelector(".dropdown").classList.remove("disable");
         document.querySelector(".saveimage").classList.remove("disable");
         document.querySelector(".slider_container").classList.remove("disable");
         document.querySelector(".foot").classList.remove("disable");
+      });
+    }
+    let filterslider=document.querySelector(".filter")
+    let values=document.querySelector(".value")
 
+    function updatefilter()
+    {
+      //values.innerHTML= `&nbsp ${filterslider.value} % `
+      const selectedfilter=document.querySelector(".dropdown .active")
+      if(selectedfilter.id === "brightness")
+      {
+        brightness=filterslider.value;
 
-
-    })
-    console.log(file)
-}
-fileInput.addEventListener("change",loadImg)
-chooseImg.addEventListener("click",()=>fileInput.click());
-
+      }
+      else if(selectedfilter.id==="saturation")
+      {
+        saturation=filterslider.value
+      }
+      else if(selectedfilter.id==="inversion")
+      {
+        inversion=filterslider.value
+      }
+      else if(selectedfilter.id==="greyscale")
+      {
+        greyscale=filterslider.value
+      }
+    }
+    fileInput.addEventListener("change", loadImg);
+    filterslider.addEventListener("input",updatefilter)
+    chooseImg.addEventListener("click", () => fileInput.click());
+  });
+  
